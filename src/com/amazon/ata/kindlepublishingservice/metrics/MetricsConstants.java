7371f342180b5c554a0303e826bcf857073defbf
umasitaraman <36041143+umasitaraman@users.noreply.github.com>
2022-05-25 13:29:32 -0400

    Creating the Repo

--- src/com/amazon/ata/kindlepublishingservice/metrics/MetricsConstants.java
+++ src/com/amazon/ata/kindlepublishingservice/metrics/MetricsConstants.java
@@ -1,2 +1,14 @@
-package com.amazon.ata.kindlepublishingservice.metrics;public class MetricsConstants {
+package com.amazon.ata.kindlepublishingservice.metrics;
+
+public class MetricsConstants {
+    public static final String NAMESPACE = "KindlePublishingServiceMetricsActivity";
+    public static final String SERVICE = "Service";
+    public static final String SERVICE_NAME = "atacurriculumkindlepublishingservice";
+    public static final String MARKETPLACE = "Marketplace";
+    public static final String US_MARKETPLACE = "US";
+
+    public static final String BOOK_RESERVATION_COUNT = "BookedReservationCount";
+    public static final String CANCELED_RESERVATION_COUNT = "CanceledReservationCount";
+    public static final String MODIFIED_RESERVATION_COUNT = "ModifiedReservationCount";
+    public static final String BOOKING_REVENUE = "ReservationRevenue";
 }
--- src/com/amazon/ata/kindlepublishingservice/metrics/MetricsPublisher.java
+++ src/com/amazon/ata/kindlepublishingservice/metrics/MetricsPublisher.java
@@ -1,2 +1,27 @@
-package com.amazon.ata.kindlepublishingservice.metrics;public class MetricsPublisher {
+package com.amazon.ata.kindlepublishingservice.metrics;
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
+
+import javax.inject.Inject;
+
+/**
+ * Contains operations for publishing metrics.
+ */
+public class MetricsPublisher {
+
+    private AmazonCloudWatch cloudWatch;
+
+    /**
+     * Creates a metrics publisher with the given AmazonCloudWatch.
+     * @param cloudWatch AmazonCloudWatch
+     */
+    @Inject
+    public MetricsPublisher (final AmazonCloudWatch cloudWatch) {
+        this.cloudWatch = cloudWatch;
+    }
+
 }
